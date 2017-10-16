b342eac11c69afbb34e543fdceb5fa263b0ba666
qpc4ever <qpc4ever@gmail.com>
2017-10-16 02:27:09 -0400

    end of sunday night commit

--- cmcglynn-bugTracker/App_Start/RouteConfig.cs
+++ cmcglynn-bugTracker/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace cmcglynn_bugTracker
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Projects", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
